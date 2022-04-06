f44a1f199493b77f18cd3533f4527c3f713d325e
vignesh24g <vignesh24.gv@gmail.com>
2022-04-06 17:08:02 +0530

    day31

---
+++ class Solution {     public int numberOfMatches(int n) {         int num=0;                      while(n!=1){                 if(n%2==0){                 num+=n/2;                 n=n/2;                 }                 else{                     num+=(n-1)/2;                     n=(n-1)/1688. Count of Matches in Tournament.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numberOfMatches(int n) {
+        int num=0;
+        
+            while(n!=1){
+                if(n%2==0){
+                num+=n/2;
+                n=n/2;
+                }
+                else{
+                    num+=(n-1)/2;
+                    n=(n-1)/2+1;
+                }
+        }
+        return num;
+        
+    }
+}
